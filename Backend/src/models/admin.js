81ac703a292bce82f4c4d3243209c48e357ea054
zaidmirza24 <mirzamohdzaid72@gmail.com>
2025-08-25 01:02:12 +0530

    ...

--- Backend/src/models/admin.js
+++ Backend/src/models/admin.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const adminSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
-  passwordHash: { type: String, required: true }
+  password: { type: String, required: true }
 }, { timestamps: true });
 
 export default mongoose.model("Admin", adminSchema);
